e9c0a1de278c180e2f046ee2b175d86f1256e5a7
Jenny Nguyen <jennayx14@yahoo.com>
2024-12-08 22:55:50 -0800

    added css to landing page

--- src/LandingPage/TOC.tsx
+++ src/LandingPage/TOC.tsx
@@ -1,30 +1,40 @@
 import { useLocation } from "react-router";
 import { Link } from "react-router-dom";
+import "../main.css";
 
 export default function TOC() {
   const { pathname } = useLocation();
     return (
-      <ul className="nav nav-pills">
-      <li className="nav-item">
-        <a id="wd-a" href="#/LandingPage" className="nav-link">
-          Home
-        </a>
-      </li>
-      <li className="nav-item">
-        <a id="wd-k" href="#/Kanbas" className="nav-link">
-          Kanbas
-        </a>
-      </li>
-      <li className="nav-item">
-        <a id="wd-k" href="https://github.com/jennyncodes/cs-5610-frontend" className="nav-link">
-          React.js Github Project
-        </a>
-      </li>
-      <li className="nav-item">
-        <a id="wd-k" href="https://github.com/jennyncodes/cs-5610-server" className="nav-link">
-          Node.js Github Project
-        </a>
-      </li>
-    </ul>
+        <div className="navmenu">
+            <nav id="navmenu" className="navmenu">
+        <ul className="nav nav-pills">
+            <li className="nav-item">
+                <a id="wd-a" href="#/LandingPage" className="nav-link">
+                Home
+                </a>
+            </li>
+
+            <li className="nav-item">
+                <a id="wd-k" href="#/Kanbas" className="nav-link">
+                Kanbas
+                </a>
+            </li>
+            <li className="dropdown"><a href="#"><span>Github Project</span> <i className="bi bi-chevron-down toggle-dropdown"></i></a>
+                <ul>
+                    <li><a href="https://github.com/jennyncodes/cs-5610-frontend">React.js Project</a>
+                    </li>
+                        <li className="dropdown">
+                            <a href="https://github.com/jennyncodes/cs-5610-server"><span>Node.js</span> 
+                            <i className="bi bi-chevron-down toggle-dropdown"></i>
+                            </a>
+                        </li>
+                </ul>
+            </li>
+    
+
+          </ul>
+          </nav>
+        </div>
+
   );
 }
--- src/LandingPage/index.tsx
+++ src/LandingPage/index.tsx
@@ -1,19 +1,57 @@
 import TOC from "./TOC";
 import { Route, Routes, Navigate } from "react-router";
 import { Provider } from "react-redux";
+import "../main.css";
+import { FaGithub } from "react-icons/fa";
 
 
 export default function LandingPage() {
   return (
-    <div id="wd-project">
-      <h1>TEAM WEB</h1>
-      <h2>Jiajie He CS 5610 Section 20593 </h2>
-      <h3>Jenny Nguyen CS 5610 Section 20593</h3>
-    
-      <TOC />
-      <Routes>
-        <Route path="/" element={<Navigate to="LandingPage" />} />
-      </Routes>
+    <div className="container">
+        <div className="header d-flex align-items-center sticky-top">
+
+            <div className="container-fluid container-xl position-relative d-flex align-items-center justify-content-between">
+                <h1 className="sitename">TeamWeb.</h1>
+            </div>
+        </div>
+        <TOC />
+        <Routes>
+            <Route path="/" element={<Navigate to="LandingPage" />} />
+        </Routes>
+
+        <div className="about section">
+            <div className="container">
+                <div className="container-fluid container-md d-flex align-items-center justify-content-between">
+                <div className="team">
+                  <h3 className="">
+                    <a href="#"><span className="">Jiajie</span> He</a>
+                  </h3>
+                  <span className="d-block position">CS 5610, Section 20593 </span>
+                  <ul className="list-unstyled social-icons light mb-3">
+                        <li>
+                            <a href="https://github.com/jiajie-he"><FaGithub /></a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div className="team">
+                    <h3 className="">
+                        <a href="#"><span className="">Jenny</span> Nguyen</a>
+                    </h3>
+                    <span className="d-block position">CS 5610, Section 20593 </span>
+
+                    <ul className="list-unstyled social-icons light mb-3">
+                        <li>
+                            <a href="https://github.com/jennyncodes"><FaGithub /></a>
+                        </li>
+                    </ul>
+
+                </div>
+
+                </div>
+            </div>
+        </div>
+
     </div>
 
 );}
